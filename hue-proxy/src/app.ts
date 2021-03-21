d3e2022b392765c79a5a8240a2db4f2a9dbd2e08
Dejan Levec <dejan@dejanlevec.com>
2021-03-21 13:04:35 +0100

    read options from file

--- hue-proxy/src/app.ts
+++ hue-proxy/src/app.ts
@@ -3,33 +3,49 @@
 import express from 'express';
 import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 import bodyParser from 'body-parser';
+import fs from 'fs';
+import util from 'util';
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/', createProxyMiddleware({
-  target: 'https://httpbin.org/',
-  changeOrigin: true,
-  onError(err, req, res) {
-    res.writeHead(500, {
-      'Content-Type': 'text/plain',
-    });
-    res.end('Something went wrong. And we are reporting a custom error message.' + err);
-  },
-  onProxyReq(proxyReq, req, res) {
-    if (req.body) {
-      var copy = JSON.parse(JSON.stringify(req.body));
+const readFile = util.promisify(fs.readFile);
 
-      if (copy.alert) {
-        delete copy.alert;
+async function main() {
+  const port = process.env.PORT ?? 30000;
+
+  const options_path = process.env.OPTIONS_PATH ?? './test_options.json';
+  console.log(`Using options path: ${options_path}`);
+
+  const options = JSON.parse((await readFile(options_path)).toString());
+
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use('/', createProxyMiddleware({
+    target: `http://${options.hue_ip}/`,
+    changeOrigin: true,
+    onError(err, req, res) {
+      res.writeHead(500, {
+        'Content-Type': 'text/plain',
+      });
+      res.end('Something went wrong. And we are reporting a custom error message.' + err);
+    },
+    onProxyReq(proxyReq, req, res) {
+      if (req.body) {
+        var copy = JSON.parse(JSON.stringify(req.body));
+
+        if (copy.alert) {
+          delete copy.alert;
+        }
+
+        const body = JSON.stringify(copy);
+
+        proxyReq.setHeader('content-length', body.length);
+        proxyReq.write(body);
+        proxyReq.end();
       }
+    },
+  }));
+  app.listen(port);
 
-      const body = JSON.stringify(copy);
+}
 
-      proxyReq.setHeader('content-length', body.length);
-      proxyReq.write(body);
-      proxyReq.end();
-    }
-  },
-}));
-app.listen(30000);
+main();
